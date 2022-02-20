c6e3192b3d701deaad204a784aa1c41b6ce937c1
Keo222 <kyote222@gmail.com>
2022-02-20 15:50:53 -0800

    modal takes up whole screen now

--- client/src/components/DeleteTrackPopup.js
+++ client/src/components/DeleteTrackPopup.js
@@ -7,9 +7,13 @@ const BlurredBackground = styled.div`
   width: 100%;
   height: 100vh;
   position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   background: rgba(0, 0, 0, 0.4);
   display: flex;
-  /* align-items: center; */
+  align-items: center;
   justify-content: center;
 `;
 const DeleteTrackModal = styled.div`
