7d655cd83f5a9996388f5b24416738e7f4893594
Keo222 <kyote222@gmail.com>
2022-02-23 16:50:45 -0800

    admin home tracks info div styling

--- client/src/components/AdminTrackInfo.js
+++ client/src/components/AdminTrackInfo.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 
 // Styled Elements
 const TrackInfoDiv = styled.div`
-  background: ${(props) => props.theme.color.textLight};
-  color: ${(props) => props.theme.color.textDark};
-  width: 30%;
+  background: ${(props) => props.theme.color.textDark};
+  color: ${(props) => props.theme.color.textLight};
+  width: 50%;
+  border: 2px solid ${(props) => props.theme.color.highlight2};
+  border-radius: 10px;
 `;
 
 const TrackInfoHeader = styled.h3`
@@ -15,17 +17,21 @@ const TrackInfoHeader = styled.h3`
   text-decoration: underline;
 `;
 
+const TrackNum = styled.p`
+  text-align: center;
+`;
+
 const StyledLink = styled(Link)`
   background: ${(props) => props.theme.color.highlight1};
-  color: inherit;
+  color: ${(props) => props.theme.color.textDark};
   font-weight: 600;
+  width: 8rem;
   text-decoration: none;
   padding: 0.8rem 2rem;
   border-radius: 5px;
   transition: all 0.3s;
-  margin: 1rem 0;
-  text-align: center;
-  display: inline-block;
+  margin: 3rem auto 1rem;
+  display: block;
   &:hover,
   &:active {
     filter: brightness(0.7);
@@ -36,8 +42,8 @@ const AdminTrackInfo = ({ numTracks }) => {
   return (
     <TrackInfoDiv>
       <TrackInfoHeader>Tracks</TrackInfoHeader>
-      <p>Number of tracks: {numTracks}</p>
-      <StyledLink to="/">Edit Tracks</StyledLink>
+      <TrackNum>Number of tracks: {numTracks}</TrackNum>
+      <StyledLink to="/admin/tracks">Edit Tracks</StyledLink>
     </TrackInfoDiv>
   );
 };
