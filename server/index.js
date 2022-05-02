82c6b065b3ee34e76c2cb233d165948b53fcdd62
Keo222 <kyote222@gmail.com>
2022-05-02 16:05:19 -0700

    changed back

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get("/api/tracks", async (req, res) => {
     res.json(allTracks.rows);
   } catch (err) {
     console.error(err.message);
-    res.status(500).body("Error getting single track");
+    res.status("Error getting single track", err);
   }
 });
 
