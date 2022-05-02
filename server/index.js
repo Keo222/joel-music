75bb5614255b5531b379f61e14dbc8f519c0e31e
Keo222 <kyote222@gmail.com>
2022-05-02 14:49:59 -0700

    changed res on back api call to updated res functionality

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get("/api/tracks", async (req, res) => {
     res.json(allTracks.rows);
   } catch (err) {
     console.error(err.message);
-    res.send("Error getting single track", err);
+    res.status(err).body("Error getting single track");
   }
 });
 
